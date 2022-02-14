5950c550385d1fae6373fa9b738fa894ae1de0d1
kim-eunji <kim-eunji@kim-eunji.local>
2022-02-14 14:39:29 +0900

    Start Project

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+
+SECRET_KEY = 'django-insecure-jxsq=ux4w*46g76v+6bus1)h6i0iy&gaoi5p_s#vy8bdl(xudi'
+
+DATABASES = {
+    'default' : {
+        'EGINE' : 'django.db.backends.mysql',
+        'NAME' : 'crud2',
+        'USER' : 'root',
+        'PASSWORD' : 'blueponny',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',          
+    }
+}
